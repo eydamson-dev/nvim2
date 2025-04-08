3a0eea8ae0eb506580bdb3b3f0eb78e20fe6496f
Adamson Vispo <eydamson.dev@gmail.com>
2025-04-08 19:55:46 +0800

    chore: remove mini nvim and re-enable barbar

--- lua/plugins/common.lua
+++ lua/plugins/common.lua
@@ -6,14 +6,5 @@ return {
 		priority = 1000, -- Very high priority is required, luarocks.nvim should run as the first plugin in your config.
 		config = true,
 	},
-	-- "nvim-tree/nvim-web-devicons",
-	-- { "echasnovski/mini.nvim", enable = false, version = "*" },
-	-- {
-	-- 	enable = false,
-	-- 	"echasnovski/mini.icons",
-	-- 	version = "*",
-	-- 	config = function()
-	-- 		require("mini.icons").setup()
-	-- 	end,
-	-- },
+	{ "nvim-tree/nvim-web-devicons", opts = {} },
 }
