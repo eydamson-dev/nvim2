a77a6760f7cdc67780074d707881d7d942773240
Adamson Vispo <eydamson.dev@gmail.com>
2025-04-08 19:35:12 +0800

    lazy: neovim-project

--- lua/plugins/project-nvim.lua
+++ lua/plugins/project-nvim.lua
@@ -1,4 +1,27 @@
 return {
-  "ahmedkhalf/project.nvim",
-  enabled = false,
+	"coffebar/neovim-project",
+	opts = {
+		dashboard_mode = true,
+		projects = { -- define project roots
+			"~/projects/*",
+			"~/.config/*",
+		},
+		picker = {
+			type = "telescope", -- or "fzf-lua"
+		},
+	},
+	init = function()
+		-- enable saving the state of plugins in the session
+		vim.opt.sessionoptions:append("globals") -- save global variables that start with an uppercase letter and contain at least one lowercase letter.
+		vim.keymap.set("n", "<C-p>", ":NeovimProjectDiscover<cr>")
+	end,
+	dependencies = {
+		{ "nvim-lua/plenary.nvim" },
+		-- optional picker
+		{ "nvim-telescope/telescope.nvim", tag = "0.1.4" },
+		-- optional picker
+		{ "Shatur/neovim-session-manager" },
+	},
+	lazy = false,
+	priority = 100,
 }
