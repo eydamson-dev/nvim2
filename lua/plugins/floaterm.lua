1d9cdee12cb91342df6550abed1981e0e59bb80d
Adamson Vispo <eydamson.dev@gmail.com>
2025-06-06 14:14:02 +0800

    floaterm: set leader + num keys to toggle or create terminal

--- lua/plugins/floaterm.lua
+++ lua/plugins/floaterm.lua
@@ -1,21 +1,32 @@
 return {
-  "voldikss/vim-floaterm",
-  config = function()
-    vim.g.floaterm_autoinsert    = 1
-    vim.g.floaterm_width         = 0.9
-    vim.g.floaterm_height        = 0.9
-    vim.g.floaterm_wintitle      = 1
-    vim.g.floaterm_autoclose     = 1
+	"voldikss/vim-floaterm",
+	config = function()
+		vim.g.floaterm_autoinsert = 1
+		vim.g.floaterm_width = 0.9
+		vim.g.floaterm_height = 0.9
+		vim.g.floaterm_wintitle = 1
+		vim.g.floaterm_autoclose = 1
 
-    local keymap = vim.keymap.set
-    keymap("n", "<leader>1", ":FloatermToggle<CR>", { noremap = true, silent = true })
-    keymap("t", "<leader>1", "<C-\\><C-n>:FloatermToggle<CR>", { noremap = true, silent = true })
-    keymap("n", "<leader>2", ":FloatermNext<CR>", { noremap = true, silent = true })
-    keymap("t", "<leader>2", "<C-\\><C-n>:FloatermNext<CR>", { noremap = true, silent = true })
-    keymap("n", "<leader>3", ":FloatermPrev<CR>", { noremap = true, silent = true })
-    keymap("t", "<leader>3", "<C-\\><C-n>:FloatermPrev<CR>", { noremap = true, silent = true })
-    keymap("n", "<leader>4", ":FloatermNew<CR>", { noremap = true, silent = true })
-    keymap("t", "<leader>4", "<C-\\><C-n>:FloatermNew<CR>", { noremap = true, silent = true })
-  end
+		local keymap = vim.keymap.set
+		local opts = { noremap = true, silent = true }
+
+		local function toggleTerminal(term_name)
+			local exist = vim.fn["floaterm#terminal#get_bufnr"](term_name)
+			if exist == -1 then
+				vim.cmd("FloatermNew --name=" .. term_name)
+			else
+				vim.cmd("FloatermToggle " .. term_name)
+			end
+		end
+
+		for i = 1, 9 do
+			local term_name = "terminal_" .. i
+			keymap("n", "<leader>" .. i, function()
+				toggleTerminal(term_name)
+			end, opts)
+
+      -- to toggle when in terminal
+			keymap("t", "<leader>" .. i, "<C-\\><C-n>:FloatermToggle " .. term_name .. "<CR>", opts)
+		end
+	end,
 }
-
