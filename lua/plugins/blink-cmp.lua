a13b2f7d7ccfd1cb2120716a9b2bc02355919264
Adamson Vispo <eydamson.dev@gmail.com>
2025-06-12 14:38:37 +0800

    chore: house keeping

--- lua/plugins/blink-cmp.lua
+++ lua/plugins/blink-cmp.lua
@@ -108,5 +108,4 @@ return {
 		-- See the fuzzy documentation for more information
 		fuzzy = { implementation = "prefer_rust_with_warning" },
 	},
-	opts_extend = { "sources.default" },
 }
--- lua/plugins/copilot.lua
+++ lua/plugins/copilot.lua
@@ -4,21 +4,5 @@ return {
 	opts = {
 		suggestion = { enabled = false },
 		panel = { enabled = false },
-		model = "gemini",
 	},
-	init = function()
-		vim.api.nvim_create_autocmd("User", {
-			pattern = "BlinkCmpMenuOpen",
-			callback = function()
-				vim.b.copilot_suggestion_hidden = true
-			end,
-		})
-
-		vim.api.nvim_create_autocmd("User", {
-			pattern = "BlinkCmpMenuClose",
-			callback = function()
-				vim.b.copilot_suggestion_hidden = false
-			end,
-		})
-	end,
 }
