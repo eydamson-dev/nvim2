0a87604fd660debbe561698538cd5d440f7802b9
adamson vispo <eydamson.dev@gmail.com>
2026-03-24 21:03:40 +0800

    fix: code folding with nvim-ufo
    - Enable foldenable (was false, preventing folding)
    - Add fold column indicators (was 0, now 1)
    - Add nice fold icons (▶ ▼) in fillchars
    - Remove broken coc.nvim reference causing errors
    - Add keymap descriptions for better UX
    - Configure treesitter foldmethod for UFO compatibility

--- lua/plugins/nvim-ufo.lua
+++ lua/plugins/nvim-ufo.lua
@@ -1,56 +1,64 @@
 return {
-	"kevinhwang91/nvim-ufo",
-	dependencies = "kevinhwang91/promise-async",
-	config = function()
-		vim.o.foldcolumn = "0" -- '0' is not bad
-		vim.o.foldlevel = 99-- Using ufo provider need a large value, feel free to decrease the value
-		vim.o.foldlevelstart = 99
-		vim.o.foldenable = false
+  "kevinhwang91/nvim-ufo",
+  dependencies = "kevinhwang91/promise-async",
+  config = function()
+    -- UFO folding settings
+    vim.o.foldcolumn = "1"
+    vim.o.foldlevel = 99
+    vim.o.foldlevelstart = 99
+    vim.o.foldenable = true
 
-		local ftMap = {
-			vim = "indent",
-			python = { "indent" },
-			git = "",
-		}
+    -- Using ufo provider need a large value, feel free to decrease the value
+    vim.opt.fillchars = {
+      fold = " ",
+      foldopen = "▼",
+      foldsep = " ",
+      foldclose = "▶",
+    }
 
-		local ufo = require("ufo")
-		ufo.setup({
-			open_fold_hl_timeout = 150,
-			close_fold_kinds_for_ft = {
-				default = { "imports", "comment" },
-				json = { "array" },
-				c = { "comment", "region" },
-			},
-			preview = {
-				win_config = {
-					border = { "", "─", "", "", "", "─", "", "" },
-					winhighlight = "Normal:Folded",
-					winblend = 0,
-				},
-				mappings = {
-					scrollU = "<C-u>",
-					scrollD = "<C-d>",
-					jumpTop = "[",
-					jumpBot = "]",
-				},
-			},
-			provider_selector = function(bufnr, filetype, buftype)
-				-- if you prefer treesitter provider rather than lsp,
-				return ftMap[filetype] or {'treesitter', 'indent'}
-			end,
-		})
+    local ftMap = {
+      vim = "indent",
+      python = { "indent" },
+      git = "",
+    }
 
-		vim.keymap.set("n", "zR", ufo.openAllFolds)
-		vim.keymap.set("n", "zM", ufo.closeAllFolds)
-		vim.keymap.set("n", "zr", ufo.openFoldsExceptKinds)
-		vim.keymap.set("n", "zm", ufo.closeFoldsWith) -- closeAllFolds == closeFoldsWith(0)
-		vim.keymap.set("n", "L", function()
-			local winid = ufo.peekFoldedLinesUnderCursor()
-			if not winid then
-				-- choose one of coc.nvim and nvim lsp
-				vim.fn.CocActionAsync("definitionHover") -- coc.nvim
-				vim.lsp.buf.hover()
-			end
-		end)
-	end,
+    local ufo = require("ufo")
+    ufo.setup({
+      open_fold_hl_timeout = 150,
+      close_fold_kinds_for_ft = {
+        default = { "imports", "comment" },
+        json = { "array" },
+        c = { "comment", "region" },
+      },
+      preview = {
+        win_config = {
+          border = { "", "─", "", "", "", "─", "", "" },
+          winhighlight = "Normal:Folded",
+          winblend = 0,
+        },
+        mappings = {
+          scrollU = "<C-u>",
+          scrollD = "<C-d>",
+          jumpTop = "[",
+          jumpBot = "]",
+        },
+      },
+      provider_selector = function(bufnr, filetype, buftype)
+        -- if you prefer treesitter provider rather than lsp,
+        return ftMap[filetype] or { "treesitter", "indent" }
+      end,
+    })
+
+    -- Keymaps
+    vim.keymap.set("n", "zR", ufo.openAllFolds, { desc = "Open all folds" })
+    vim.keymap.set("n", "zM", ufo.closeAllFolds, { desc = "Close all folds" })
+    vim.keymap.set("n", "zr", ufo.openFoldsExceptKinds, { desc = "Open folds except kinds" })
+    vim.keymap.set("n", "zm", ufo.closeFoldsWith, { desc = "Close folds with" }) -- closeAllFolds == closeFoldsWith(0)
+    vim.keymap.set("n", "L", function()
+      local winid = ufo.peekFoldedLinesUnderCursor()
+      if not winid then
+        vim.lsp.buf.hover()
+      end
+    end, { desc = "Peek fold or LSP hover" })
+  end,
 }
--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -1,21 +1,25 @@
 return {
-	"nvim-treesitter/nvim-treesitter",
-	build = ":TSUpdate",
-	config = function()
-		local status_ok, configs = pcall(require, "nvim-treesitter.configs")
-		if not status_ok then
-			return
-		end
+  "nvim-treesitter/nvim-treesitter",
+  build = ":TSUpdate",
+  config = function()
+    local status_ok, configs = pcall(require, "nvim-treesitter.configs")
+    if not status_ok then
+      return
+    end
 
-		configs.setup({
-			modules = {},
-			ignore_install = {},
-			ensure_installed = { "lua", "css", "scss", "html", "javascript", "typescript", "json" },
-			sync_install = false,
-			auto_install = true,
-			highlight = {
-				enable = true,
-			},
-		})
-	end,
+    configs.setup({
+      modules = {},
+      ignore_install = {},
+      ensure_installed = { "lua", "css", "scss", "html", "javascript", "typescript", "json" },
+      sync_install = false,
+      auto_install = true,
+      highlight = {
+        enable = true,
+      },
+    })
+
+    -- Set up treesitter folding for UFO
+    vim.opt.foldmethod = "manual"
+    vim.opt.foldexpr = "nvim_treesitter#foldexpr()"
+  end,
 }
